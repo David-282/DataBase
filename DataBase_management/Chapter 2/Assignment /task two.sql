f1165d9aa3a6550c88657df8e6a244c37cb9995c
David-282 <afolabidavid94@gmail.com>
2026-04-22 07:46:37 +0100

    second commit

--- DataBase_management/Chapter 2/Assignment /task two.sql
+++ DataBase_management/Chapter 2/Assignment /task two.sql
@@ -55,6 +55,7 @@ FROM CUSTOMER
 GROUP BY LastName, FirstName;
 
 
+-- M
 SELECT LastName, FirstName, Phone
 FROM CUSTOMER
 WHERE CustomerID IN (
@@ -63,3 +64,52 @@ WHERE CustomerID IN (
     WHERE TotalAmount > 100.00
 )
 ORDER BY LastName ASC, FirstName DESC;
+
+-- n
+SELECT DISTINCT c.LastName, c.FirstName, c.Phone
+FROM Customers c, Orders o
+WHERE c.CustomerID = o.CustomerID
+  AND o.TotalAmount > 100.00
+ORDER BY c.LastName ASC, c.FirstName DESC;
+
+
+-- o
+SELECT DISTINCT c.LastName, c.FirstName, c.Phone
+FROM CUSTOMER c
+JOIN INVOICE i
+    ON c.CustomerID = i.CustomerID
+WHERE i.TotalAmount > 100.00
+ORDER BY c.LastName ASC, c.FirstName DESC;
+
+
+-- p
+SELECT LastName, FirstName, Phone
+FROM CUSTOMER
+WHERE CustomerID IN (
+    SELECT CustomerID
+    FROM INVOICE
+    WHERE InvoiceNumber IN (
+        SELECT InvoiceNumber
+        FROM INVOICE_ITEM
+        WHERE Item = 'Dress Shirt'
+    )
+)
+ORDER BY LastName ASC, FirstName DESC;
+
+
+SELECT DISTINCT c.LastName, c.FirstName, c.Phone
+FROM CUSTOMER c, INVOICE i, INVOICE_ITEM ii
+WHERE c.CustomerID = i.CustomerID
+  AND i.InvoiceNumber = ii.InvoiceNumber
+  AND ii.Item = 'Dress Shirt'
+ORDER BY c.LastName ASC, c.FirstName DESC;
+
+
+SELECT DISTINCT c.LastName, c.FirstName, c.Phone
+FROM CUSTOMER c
+JOIN INVOICE i
+    ON c.CustomerID = i.CustomerID
+JOIN INVOICE_ITEM ii
+    ON i.InvoiceNumber = ii.InvoiceNumber
+WHERE ii.Item = 'Dress Shirt'
+ORDER BY c.LastName ASC, c.FirstName DESC;
